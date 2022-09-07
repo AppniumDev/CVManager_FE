dd6771ab6bbcb49768eea641aba272564642279d
Davide <davide.cuccato5@gmail.com>
2022-09-07 18:48:45 +0200

    Test commit

--- components/BaseLayout.tsx
+++ components/BaseLayout.tsx
@@ -5,14 +5,10 @@ const BaseLayout = ({ children }: any) => {
     <>
       <div className="flex flex-no-wrap">
         {/* Sidebar starts */}
-        {/* Remove class [ hidden ] and replace [ sm:flex ] with [ flex ] */}
 
         <SideMenu />
 
-        {/* Sidebar ends */}
-        {/* Remove class [ h-64 ] when adding a card block */}
         <div className="container mx-auto py-10 h-64 md:w-4/5 w-11/12 px-6">
-          {/* Remove class [ border-dashed border-2 border-gray-300 ] to remove dotted border */}
           <div className="w-full h-full rounded border-dashed border-2 border-gray-300">
             {children}
           </div>
